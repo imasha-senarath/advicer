9317aa697324b1516a2611e06e3aa5258a7f9bc7
Imasha Senarath <imashadsenarath@gmail.com>
2024-11-17 21:32:00 +0530

    Project setup.

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:advicer/application/pages/advice/advice_page.dart';
+import 'package:advicer/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'application/core/services/theme_service.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeService(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeService>(builder: (context, themeService, child) {
+      return MaterialApp(
+        themeMode: themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        home: const AdvicePage(),
+      );
+    });
+  }
+}
